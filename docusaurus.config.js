a0d28c835a8e453d29fd87f4569bcaadca34e172
Nigel Ritfeld <nigelritfeld@live.nl>
2022-06-08 12:16:16 +0200

    Corrected urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Unlimited HR`,
       },
       prism: {
         theme: lightCodeTheme,
